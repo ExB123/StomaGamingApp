da8a181f277e041dcba7675bac27db693657a314
ExB123 <103373284+ExB123@users.noreply.github.com>
2026-05-07 14:21:14 +0300

    Restored App.jsx and fixed project structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-bg">
-      <h1 className="text-2xl font-bold text-accent">StomaGaming React запущен! 🚀</h1>
-    </div>
-  )
-}
-
-export default App
